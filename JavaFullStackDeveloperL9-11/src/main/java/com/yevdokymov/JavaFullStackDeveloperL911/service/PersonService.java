5fcd98e919eb005e20b8489eaceb29005cf2356c
Bohdan Yevdokymov <103249940+fucusifei@users.noreply.github.com>
2023-01-18 19:37:29 +0200

    Delete JavaFullStackDeveloperL9-11 directory

--- JavaFullStackDeveloperL9-11/src/main/java/com/yevdokymov/JavaFullStackDeveloperL911/service/PersonService.java
+++
@@ -1,7 +0,0 @@
-package com.yevdokymov.JavaFullStackDeveloperL911.service;
-
-import com.yevdokymov.JavaFullStackDeveloperL911.model.PersonThatSheltered;
-
-public interface PersonService {
-    PersonThatSheltered createPerson(PersonThatSheltered person);
-}
