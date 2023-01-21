0b37467bdf130f5df526d5a6a2b99f04e00c0963
Bohdan Yevdokymov <103249940+fucusifei@users.noreply.github.com>
2023-01-21 11:33:54 +0200

    Delete JavaFullStackDeveloperL9-11 directory

--- JavaFullStackDeveloperL9-11/src/main/java/com/yevdokymov/JavaFullStackDeveloperL911/repository/PersonRepository.java
+++
@@ -1,9 +0,0 @@
-package com.yevdokymov.JavaFullStackDeveloperL911.repository;
-
-import com.yevdokymov.JavaFullStackDeveloperL911.model.PersonThatSheltered;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PersonRepository extends JpaRepository<PersonThatSheltered,Long> {
-}
